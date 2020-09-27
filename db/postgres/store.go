367e0c771b5d90a7c42521118603c7e039853b76
Gabriel Gurgel <37349749+CheckRates@users.noreply.github.com>
2020-09-26 21:15:22 -0400

    ✨ Add Image post transaction

--- db/postgres/store.go
+++ db/postgres/store.go
@@ -1,6 +1,8 @@
 package postgres
 
 import (
+	"context"
+	"errors"
 	"fmt"
 
 	"github.com/checkrates/Fime/fime"
@@ -9,6 +11,7 @@ import (
 
 // NewStore returns all the data access points of Fime
 func NewStore(dataSource string) (*Store, error) {
+	// Open db and connect to it
 	db, err := sqlx.Open("postgres", dataSource)
 	if err != nil {
 		return nil, fmt.Errorf("error opening database: %w", err)
@@ -19,15 +22,99 @@ func NewStore(dataSource string) (*Store, error) {
 
 	// Return the wrapper of the DB
 	return &Store{
-		ImageStore: NewImageStore(db),
-		UserStore:  NewUserStore(db),
-		TagStore:   NewTagStore(db),
+		db:            db,
+		ImageStore:    NewImageStore(db),
+		UserStore:     NewUserStore(db),
+		TagStore:      NewTagStore(db),
+		ImageTagStore: NewImageTagStore(db),
 	}, nil
 }
 
-// Store contain all the data access points
+// Store contain all the data access points to the tables of Fime
 type Store struct {
+	db *sqlx.DB
 	fime.UserStore
 	fime.ImageStore
 	fime.TagStore
+	fime.ImageTagStore
+}
+
+func (store *Store) execTx(ctx context.Context, fn func(*Store) error) error {
+	tx, err := store.db.BeginTxx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	err = fn(store)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			allErrors := "Transaction Error: " + rbErr.Error() + "\n" + err.Error()
+			return errors.New(allErrors)
+		}
+		return err
+	}
+
+	return tx.Commit()
+}
+
+/*
+ * 	Post an Image Transaction
+ */
+
+// MakePostParams provides all data for creating a image post in Fime
+type MakePostParams struct {
+	Name   string     `json:"name"`
+	URL    string     `json:"url"`
+	UserID int64      `json:"ownerID"`
+	Tags   []fime.Tag `json:"tags"`
+}
+
+// MakePostResult is the result of creating a post
+type MakePostResult struct {
+	Image fime.Image `json:"image"`
+	Tags  []fime.Tag `json:"tags"`
+}
+
+// MakePostTx creates a image post in the database
+func (store *Store) MakePostTx(ctx context.Context, arg MakePostParams) (MakePostResult, error) {
+	var retPost MakePostResult
+
+	err := store.execTx(ctx, func(store *Store) error {
+		var err error
+
+		// Create Image
+		newImg := fime.Image{
+			Name:    arg.Name,
+			URL:     arg.URL,
+			OwnerID: arg.UserID,
+		}
+		err = store.CreateImage(&newImg)
+		if err != nil {
+			return err
+		}
+
+		// Create Tags -> Tags that already exist are returned
+		for i := 0; i < len(arg.Tags); i++ {
+			if err = store.CreateTag(&arg.Tags[i]); err != nil {
+				return err
+			}
+
+			// Associate tags with image
+			imgTag := fime.ImageTag{
+				ImageID: newImg.ID,
+				TagID:   arg.Tags[i].ID,
+			}
+			if err = store.CreateImageTag(imgTag); err != nil {
+				return err
+			}
+		}
+
+		retPost.Image = newImg
+		retPost.Tags = arg.Tags
+
+		return nil
+	})
+
+	// Return created post and error
+	return retPost, err
 }
--- db/postgres/store_test.go
+++ db/postgres/store_test.go
@@ -1,12 +1,16 @@
 package postgres
 
 import (
+	"context"
 	"log"
 	"os"
 	"testing"
 
 	"github.com/checkrates/Fime/config"
+	"github.com/checkrates/Fime/fime"
+	"github.com/checkrates/Fime/util"
 	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/require"
 )
 
 var dal *Store
@@ -22,4 +26,32 @@ func init() {
 func TestMain(m *testing.M) {
 	dal.User(1)
 	os.Exit(m.Run())
+}
+
+func TestMakePostTx(t *testing.T) {
+	user := createTestUser(t)
+	var tags []fime.Tag
+	for i := 0; i < 3; i++ {
+		newTag := fime.Tag{
+			Name: util.RandomString(4),
+		}
+		tags = append(tags, newTag)
+	}
+
+	postArgs := MakePostParams{
+		Name:   "IMG_2020",
+		URL:    "www.coolImage.com",
+		UserID: user.ID,
+		Tags:   tags,
+	}
+
+	newPost, err := dal.MakePostTx(context.Background(), postArgs)
+
+	require.NoError(t, err)
+	require.NotZero(t, newPost.Image.ID)
+
+	require.Equal(t, newPost.Image.Name, postArgs.Name)
+	require.Equal(t, newPost.Image.URL, postArgs.URL)
+	require.Equal(t, newPost.Image.OwnerID, postArgs.UserID)
+	require.Equal(t, newPost.Tags, postArgs.Tags)
 }
