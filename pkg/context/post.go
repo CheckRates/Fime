dc2f7f4bba7bccdc054adac03c451b8e62f522ee
CheckRates <gabsantana23@gmail.com>
2023-08-29 23:13:51 -0400

    :bug: Add post error check

--- pkg/context/post.go
+++ pkg/context/post.go
@@ -10,16 +10,24 @@ import (
 )
 
 // Returns a new instance of the post usecase
-func NewPostUsecase(db *sqlx.DB, region, bucketName, accessId, secret string) service.PostUsecase {
+func NewPostUsecase(db *sqlx.DB, region, bucketName, accessId, secret string) (service.PostUsecase, error) {
+	bucket, err := bucket.NewS3Service(region, bucketName, accessId, secret)
+	if err != nil {
+		return nil, err
+	}
+
 	return post.NewPostService(
 		postgres.NewPostRepository(db),
-		bucket.NewS3Service(region, bucketName, accessId, secret),
-	)
+		bucket,
+	), nil
 }
 
 // Returns a new HTTP implementation of the postPort
-func NewPostPort(db *sqlx.DB, region, bucket, accessId, secret string) http.PostPort {
-	return http.NewPostApi(
-		NewPostUsecase(db, region, bucket, accessId, secret),
-	)
+func NewPostPort(db *sqlx.DB, region, bucket, accessId, secret string) (http.PostPort, error) {
+	post, err := NewPostUsecase(db, region, bucket, accessId, secret)
+	if err != nil {
+		return nil, err
+	}
+
+	return http.NewPostApi(post), nil
 }
